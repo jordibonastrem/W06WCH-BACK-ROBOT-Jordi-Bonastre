0103ead117e169707920b1a1c01124f230b0245d
Jordi <jordi.bonastre.m@gmail.com>
2021-11-07 11:24:15 +0100

    add getRobotById controller

--- server/controller/robotsController.js
+++ server/controller/robotsController.js
@@ -5,6 +5,24 @@ const getRobots = async (req, res) => {
   res.json(robots);
 };
 
+const getRobotById = async (req, res, next) => {
+  const { _id } = req.params;
+  try {
+    const searchedPet = await Robot.findById(_id);
+    if (searchedPet) {
+      res.json(searchedPet);
+    } else {
+      const error = new Error("Robot not found");
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+};
+
 module.exports = {
   getRobots,
+  getRobotById,
 };
